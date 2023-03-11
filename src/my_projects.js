ba91903b9d33ec8fbf39f711afc34cfa136b2efd
Sadra Etaei <etaeisadra@gmail.com>
2023-03-11 16:14:04 +0330

    made a few changes

---
+++ src/my_projects.js
@@ -0,0 +1,27 @@
+ import img1 from "./images/Web capture_4-3-2023_72834_.jpeg"
+ import img2 from "./images/Web capture_4-3-2023_71333_.jpeg"
+ import img3 from "./images/Web capture_4-3-2023_72659_127.0.0.1.jpeg"
+ import img4 from "./images/Web capture_4-3-2023_71548_.jpeg"
+ const data = [
+    {
+        src : img1 , 
+        name : "loop studios landing page",
+        link : ""
+    } ,
+    {
+        src : img2 , 
+        name : "ecommerce page",
+        link : "https://github.com/sadra-etaei/ecommerce-page"
+    } ,
+    {
+        src : img3, 
+        name : "xlog",
+        link : "https://github.com/sadra-etaei/xlog"
+    } ,
+    {
+        src : img4 , 
+        name : "space tourism landing page",
+        link : "https://github.com/sadra-etaei/space-tourism-project"
+    } 
+]
+export default data
