a45ab703bb75a87e4e402b5fcd2c4a309a9a8d80
sadra-etaei <sadra.etaei.1386@gmail.com>
2026-08-03 14:03:38 +0300

    added a project

--- src/config.js
+++ src/config.js
@@ -23,6 +23,14 @@ When I'm not coding, you'll find me reading papers, experimenting with new archi
 
 export const projects = [
   {
+    title: 'BERT',
+    description:
+      'a reimplementation of BERT',
+    url: 'https://github.com/sadra-etaei/BERT',
+    tags: ['NLP', 'pretraining'],
+    year: '2026',
+  },
+  {
     title: 'img-transformer',
     description:
       'a reimplementation of the image transformer paper , implemented 2D local Attention and a vision transformer',
